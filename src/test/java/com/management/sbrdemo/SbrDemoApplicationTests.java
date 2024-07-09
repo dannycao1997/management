556ec738fa6043017a73b3d13894fc67972aaf75
Danny Cao <dannycao1997@yahoo.com>
2024-07-09 18:34:01 -0400

    install spring and dependencies

---
+++ src/test/java/com/management/sbrdemo/SbrDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.sbrdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
